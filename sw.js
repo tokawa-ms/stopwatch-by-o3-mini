fd823bab450177b6a5950e870dfa3bd7af1329d4
Takashi Okawa <tokawa@microsoft.com>
2025-03-07 00:51:11 +0900

    pwa化

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'stopwatch-cache-v1';
+const urlsToCache = [
+  '/src/index.html',
+  '/src/manifest.json'
+  // ...必要に応じて他のアセットも追加...
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
